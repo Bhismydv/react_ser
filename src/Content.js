a2bca20a0cbf20d2d580d753715dbd207ab8af38
bhismydv <bhismydv2912@gmail.com>
2025-04-23 16:39:45 +0530

    components, inline css, function, import

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+const Content = () => {
+    const handleNameChange = () => {
+        const name = ["Bhism", "Anas", "Nitin"];
+        const int = Math.floor(Math.random()*3);
+        return name[int];
+      }
+    return (
+        <main>
+            <p>Hello {handleNameChange()}!</p>
+        </main>
+    )
+}
+
+export default Content
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    const headerStyle = {
+        backgroundColor: 'mediumaquamarine',
+        color: '#fff'
+    };
+    return (
+        <header style={headerStyle}>
+            <h1>Groceries List</h1>
+        </header>
+    )
+}
+
+export default Header
