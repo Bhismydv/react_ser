39141cb97e0e74dd68ac44687ee25c850e6137dd
bhismydv <bhismydv2912@gmail.com>
2025-04-23 18:15:59 +0530

    onClick

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,26 @@ const Content = () => {
         const int = Math.floor(Math.random()*3);
         return name[int];
       }
+
+      const handleClick = () => {
+        console.log("You Clicked Here!")
+      }
+    
+      const handleClick2 = (name1) => {
+        console.log(`${name1} was clicked`)
+      }
+
+      const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+      }
+
     return (
         <main>
-            <p>Hello {handleNameChange()}!</p>
+        
+            <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={()=>handleClick2('Bhism')}>Click On</button>
+            <button onClick={(e)=>handleClick3(e)}>Click On</button>
         </main>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,7 @@
 const Header = () => {
-    const headerStyle = {
-        backgroundColor: 'mediumaquamarine',
-        color: '#fff'
-    };
+   
     return (
-        <header style={headerStyle}>
+        <header>
             <h1>Groceries List</h1>
         </header>
     )
