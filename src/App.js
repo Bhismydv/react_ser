b3a67d560f3f0ce663dcb11d8d010eff280e9ea8
bhismydv <bhismydv2912@gmail.com>
2025-04-27 21:29:07 +0530

    crud

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import { useState, useEffect, use } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
+import apiRequest from './apiRequest';
 
 function App() {
   const API_URL = "http://localhost:3500/items";
@@ -24,7 +25,6 @@ useEffect(()=>{
       const response = await fetch(API_URL);
       if(!response.ok)throw Error('did not reveived expected data!');
       const listItems = await response.json();
-      console.log(listItems);
       setItems(listItems);
       setFetchError(null);
     } catch (err) {
@@ -39,22 +39,46 @@ useEffect(()=>{
  
 }, [])
 
-const addItem = (item) =>{
-  const id = items.length ? items[items.length-1].id+1 : 1
+const addItem = async(item) =>{
+  const id = items.length ? items[items.length-1].id + 1 : 1
   const myNewItem = {id, checked: false, item};
   const listItems = [...items, myNewItem]; 
-  setItems(listItems)}
+  setItems(listItems);
 
-const handleCheck =(id)=>{
+  const postOptions ={
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(myNewItem)
+  }
+  const result = await apiRequest(API_URL, postOptions);
+  if(result) setFetchError(result);
+}
+
+const handleCheck = async (id)=>{
   const listItems =items.map((item)=>item.id === id ?
       {...item, checked: !item.checked} : item
   );
   setItems(listItems);
+
+  const myItem = listItems.filter((item) => item.id===id);
+  const updateOptions = {
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({checked: myItem[0].checked})
+  };
+  const reqUrl = `${API_URL}/${id}`;
+  const result = await apiRequest(reqUrl, updateOptions);
+  if(result) setFetchError(result);
 }
 
-const handleDelete = (id)=>{
+const handleDelete =async (id)=>{
  const listItems = items.filter((item)=>item.id!==id);
  setItems(listItems);
+
+ const deleteOptions = {method: 'DELETE'};
+ const reqUrl = `${API_URL}/${id}`;
+ const result = await apiRequest(reqUrl, deleteOptions);
+ if(result) setFetchError(result);
 }
 
 const handleSubmit = (e)=>{
